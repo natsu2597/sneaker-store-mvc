62157410827e7f3e841501cb6f8e7b24bdc7e628
natsu2597 <atulrishi1997@gmail.com>
2026-07-19 01:54:01 +0530

    change password view created

---
+++ src/SneakerStore.Services/wwwroot/js/passwordToggle.js
@@ -0,0 +1,23 @@
+﻿document.querySelectorAll(".password-toggle").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const input = document.getElementById(button.dataset.target);
+        const icon = button.querySelector("i");
+
+        if (input.type === "password") {
+
+            input.type = "text";
+            icon.classList.replace("bi-eye", "bi-eye-slash");
+        }
+
+        else {
+
+            input.type = "password";
+            icon.classList.replace("bi-eye-slash", "bi-eye");
+        }
+    })
+
+    
+
+})
